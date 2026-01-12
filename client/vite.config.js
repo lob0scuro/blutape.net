5b9891a6495f1bd1ce40c56b4aa3a093a4279998
lob0scuro <kamrin717@gmail.com>
2026-01-12 15:13:59 -0600

    PWA settings applied

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 const SERVER = process.env.SERVER_URL;
 
@@ -12,6 +13,34 @@ export default defineConfig(({ mode }) => {
       react({
         babel: {
           plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: [
+          "blu-logo-192.png",
+          "blu-logo-512.png",
+          "blu-logo-apple-icon.png",
+        ],
+        manifest: {
+          name: "bluTape",
+          short_name: "bluTape",
+          start_url: "/",
+          display: "standalone",
+          background_color: "#8999af",
+          theme_color: "#262f4c",
+          icons: [
+            {
+              src: "/blu-logo-192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/blu-logo-512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
         },
       }),
     ],
